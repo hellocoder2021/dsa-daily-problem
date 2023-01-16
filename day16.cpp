f9878c4d047316d73afade781d446febe5e6e7f8
bigbull_coder <prashantj121999@gmail.com>
2023-01-16 22:39:50 +0530

    POTD 16

---
+++ day16.cpp
@@ -0,0 +1,24 @@
+int guess(int num);
+class Solution {
+public:
+    int guessNumber(int n) {
+        int i=1,j=n;
+        while(i<=j)
+        {
+            int mid=i+(j-i)/2;
+            if(guess(mid) == 0)
+            {
+                return mid;
+            }
+            else if(guess(mid) == 1)
+            {
+                i=mid+1;
+            }
+            else
+            {
+                j=mid-1;
+            }
+        }
+        return -1;
+    }
+};
